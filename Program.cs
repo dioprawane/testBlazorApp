6e33a355913c47b7492b66b0512fa25fd5a2e66c
aka ioda <53943689+akaioda@users.noreply.github.com>
2025-04-24 18:01:25 +0200

    Mise en place des composants de base pour l'auth windows, ajout de Radzen et configuration. Création de l'espace Pages/ToBeRemoved pour des éléments de test.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
 using GestionBudgétaire.Components;
+// JQ : pour Introduction du SSO windows
+// il faut ajouter Microsoft.AspNetCore.Authentication.Negotiate via nuget
+using Microsoft.AspNetCore.Authentication.Negotiate;
+using Radzen;
 
 namespace GestionBudgétaire
 {
@@ -7,6 +11,20 @@ namespace GestionBudgétaire
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
+            // JQ : Ajout de l'authentification Windows (Negotiate)
+            builder.Services.AddAuthentication(NegotiateDefaults.AuthenticationScheme)
+                .AddNegotiate();
+            builder.Services.AddAuthorization(options =>
+            {
+                //  JQ : Toutes les pages exigent une authentification par défaut
+                options.FallbackPolicy = options.DefaultPolicy;
+            });
+            //  JQ : Nécessaire pour le composant de revalidation du password AD
+            builder.Services.AddHttpContextAccessor();
+
+            // JQ : ajout pour l'utilisation de RadZen
+            builder.Services.AddRadzenComponents();
 
             // Add services to the container.
             builder.Services.AddRazorComponents()
@@ -23,6 +41,10 @@ namespace GestionBudgétaire
             }
 
             app.UseHttpsRedirection();
+
+            // JQ : Activation de l'authentification et de l'autorisation
+            app.UseAuthentication();
+            app.UseAuthorization();
 
             app.UseStaticFiles();
             app.UseAntiforgery();
