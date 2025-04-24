573d03518949d8223c9420bcdb413de5bba94d60
DIOP Serigne-Rawane (Acoss) <serigne-rawane.diop@acoss.fr>
2025-04-24 16:10:36 +0200

    Système de Logs : OK

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,6 +1,18 @@
-﻿namespace GestionBudgétaire.Data
+﻿using GestionBudgétaire.Data.Entities.Database;
+using Microsoft.EntityFrameworkCore;
+
+namespace GestionBudgétaire.Data
 {
-    public class ApplicationDbContext
+    public class ApplicationDbContext : DbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        // Propriete DbSet<Dialogue> representant la table Dialogue dans la base de donn�es
+        // Cette propri�t� permet de faire des requ�tes et des op�rations CRUD sur la table Dialogue
+        public DbSet<Dialogue>? Dialogue { get; set; }
+
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
 using GestionBudgétaire.Components;
+using GestionBudgétaire.Data;
+using Microsoft.EntityFrameworkCore;
+using Serilog.Events;
+using Serilog;
 
 namespace GestionBudgétaire
 {
@@ -8,9 +12,42 @@ namespace GestionBudgétaire
         {
             var builder = WebApplication.CreateBuilder(args);
 
+            // Configuration de Serilog
+            Log.Logger = new LoggerConfiguration()
+                // Définir le niveau de log minimum pour tout le système à Debug
+                .MinimumLevel.Debug()
+                // Surcharger le niveau de log minimum pour les logs provenant de l'espace de noms "Microsoft" à Information
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+                // Enrichir les logs avec le contexte de la log
+                .Enrich.FromLogContext()
+                // Ajouter une destination de log (sink) pour la console
+                .WriteTo.Console()
+                // Ajouter une destination de log (sink) pour un fichier
+                // Les logs seront écrits dans des fichiers sous le dossier "logs" avec un nom de fichier "log-[date].txt"
+                // Les fichiers de log seront segmentés quotidiennement (rollingInterval: RollingInterval.Day)
+                .WriteTo.File("logs/log-.txt", rollingInterval: RollingInterval.Day)
+                // Créer l'instance de logger configurée
+                .CreateLogger();
+
             // Add services to the container.
             builder.Services.AddRazorComponents()
                 .AddInteractiveServerComponents();
+
+            // Gestion de la base de donn�es
+            string? connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+            string dbPassword = Environment.GetEnvironmentVariable("PASSWORDMYSQL") ?? "default_password";
+            Console.WriteLine("Mot de passe DB récupéré: " + dbPassword);
+            if (connectionString != null)
+            {
+                connectionString = connectionString.Replace("{PASSWORD}", dbPassword);
+            }
+            else
+            {
+                throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+            }
+
+            builder.Services.AddDbContextFactory<ApplicationDbContext>(options =>
+                options.UseMySql(connectionString, new MySqlServerVersion(new Version(8, 0, 21))));
 
             var app = builder.Build();
 
@@ -30,7 +67,22 @@ namespace GestionBudgétaire
             app.MapRazorComponents<App>()
                 .AddInteractiveServerRenderMode();
 
-            app.Run();
+            //app.Run();
+            // Exécution/Lancement de l'application web
+            //app.Run();
+            try
+            {
+                // Exécution/Lancement de l'application web
+                app.Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "L'application a rencontré une erreur critique");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
     }
 }
