a9798d2de0ecbe3fabe892ef1ace72cb66be04fb
DIOP Serigne-Rawane (Acoss) <serigne-rawane.diop@acoss.fr>
2025-05-07 16:56:12 +0200

    MAJ  gitversion : ok

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,5 +1,3 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("0.0.0.0")]
-[assembly: AssemblyFileVersion("0.0.0.0")]
-[assembly: AssemblyInformationalVersion("LOCAL")]
+[assembly: CLSCompliant(true)]
