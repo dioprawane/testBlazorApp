ab1c08fd12e5da8a4904ead691ecc08b9a3057fd
DIOP Serigne-Rawane (Acoss) <serigne-rawane.diop@acoss.fr>
2025-05-11 15:52:24 +0200

    Supprimer AssemblyInfo.cs du suivi Git et l'ajouter au .gitignore

--- Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-using System.Reflection;
-
-//[assembly: CLSCompliant(true)]
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
-[assembly: AssemblyInformationalVersion("0.1.0-fusion-projets.1+96.Branch.fusion-projets.Sha.96256ce408ca3b1b8ac97a25b9aa18efd9d3f982")]
