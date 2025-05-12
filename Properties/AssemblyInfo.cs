2a2922dc934f642d560647d5c340b7e72fe2c8ba
DIOP Serigne-Rawane (Acoss) <serigne-rawane.diop@acoss.fr>
2025-05-12 15:36:44 +0200

    first commit

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+
+//[assembly: CLSCompliant(true)]
+[assembly: AssemblyVersion("0.1.0.1")]
+[assembly: AssemblyFileVersion("0.1.0.1")]
+[assembly: AssemblyInformationalVersion("0.1.0-fusion-projets.1+105.Branch.fusion-projets.Sha.b31319e8a32410c679a3354ec751610c5e5bcc7e")]
